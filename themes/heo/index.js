bee052643cfd91526eba5154c219d0dedc6c2edc
xuanmenliuyao <1601958350@qq.com>
2024-06-26 18:02:37 +0800

    loader

--- themes/heo/index.js
+++ themes/heo/index.js
@@ -41,7 +41,7 @@ import SearchNav from './components/SearchNav'
 import SideRight from './components/SideRight'
 import CONFIG from './config'
 import { Style } from './style'
-import Loader from './components/Loader'
+import Loader from './components/loader'
 
 /**
  * 基础布局 采用上中下布局，移动端使用顶部侧边导航栏
